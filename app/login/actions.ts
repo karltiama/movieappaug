515e2afceea40d64a9deccf5c7b12eeb20eb62ec
Karl Tiama <karltiama@gmail.com>
2024-08-21 23:46:57 -0400

    chore: Add utility function for merging CSS classes and update npm dependencies

---
+++ app/login/actions.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+
+export async function emaillogin(formData: FormData) {
+	const supabase = createClient();
+
+	// type-casting here for convenience
+	// in practice, you should validate your inputs
+	const data = {
+		email: formData.get("email") as string,
+		password: formData.get("password") as string,
+	};
+
+	const { error } = await supabase.auth.signInWithPassword(data);
+
+	if (error) {
+		redirect("/login?message=Could not authenticate user");
+	}
+
+	revalidatePath("/", "layout");
+	redirect("/watchlist");
+}
+
+export async function signup(formData: FormData) {
+	const supabase = createClient();
+
+	// type-casting here for convenience
+	// in practice, you should validate your inputs
+	const data = {
+		email: formData.get("email") as string,
+		password: formData.get("password") as string,
+	};
+
+	const { error } = await supabase.auth.signUp(data);
+
+	if (error) {
+		redirect("/login?message=Error Signing Up");
+	}
+
+	revalidatePath("/", "layout");
+	redirect("/login");
+}
+
+export async function signout() {
+	const supabase = createClient();
+
+	await supabase.auth.signOut();
+
+	revalidatePath("/", "layout");
+	redirect("/login");
+}
